3e2bc3e135fea6ecdd4cfc2f29a8b66def6eccce
Dillon <nerdjd@gmail.com>
2021-11-30 21:22:29 -0700

    day2 learning state in react, cleaned up some code, experimenting with navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 import "./styles.scss";
 
-function HeadBox({ content }) {
+const NavBar = ({ content }) => {
+  console.log(content);
+  let list = [];
+  content.forEach((item) => {
+    list.push(<li>{item}</li>);
+  });
+
+  return <div className="NavBar">{list}</div>;
+};
+
+const HeadBox = ({ content }) => {
   return (
-    <div>
-      <div class="hbox-1">
-        <img
-          alt="placeholder"
-          src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Feedbin-Icon-menu.svg"
-        />
+    <div className="HeadBox">
+      <div className="hbox-1">
+        <a href="#">
+          <img
+            alt="placeholder"
+            src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Feedbin-Icon-menu.svg"
+          />
+        </a>
       </div>
-      <div class="hbox-2">
+      <div className="hbox-2">
         <h1> {content.name} </h1>
+        <NavBar content={content.NavBar} />
       </div>
     </div>
   );
-}
-function TributePage({ content }) {
+};
+
+const TributePage = ({ content }) => {
   return (
     <div>
       <HeadBox content={content} />
     </div>
   );
-}
+};
+
 export default function App() {
   return <TributePage content={CONTENT} />;
 }
 
 const CONTENT = {
-  name: "elon Musk",
-  sliders: ["lorem ipsum", "ipsum lorem", "testing 12345 testing"]
+  NavBar: ["uno", "dos", "tres"],
+  name: "elon Musk"
 };
