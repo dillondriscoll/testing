9c72c5b1b01e56e5d92580fa141fd3621587dacb
Dillon <nerdjd@gmail.com>
2021-12-01 19:28:20 -0700

    images + navbar seperation seperated navbar to its own component - added images to data object and main content

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import { useState } from "react";
 const MainContent = (props) => {
   const contentList = [];
   props.contentText.forEach((item, index) => {
+    const contentImage = props.contentImage[index];
+
     const idName = props.contentID[index];
-    contentList.push(<div id={idName}>{item}</div>);
+    if (contentImage) {
+      contentList.push(
+        <div id={idName}>
+          <img src={contentImage} alt="test"></img>
+          <br />
+          {item}
+        </div>
+      );
+    } else {
+      contentList.push(<div id={idName}>{item}</div>);
+    }
   });
   return <div className="mainContent">{contentList}</div>;
 };
 
 const NavBar = (props) => {
+  const [visible, setVisible] = useState(false);
   const list = [];
+
   props.names.forEach((item, index) => {
     let linksItem = props.links[index];
     list.push(
@@ -21,29 +35,25 @@ const NavBar = (props) => {
     );
   });
 
-  return <div className="NavBar">{list}</div>;
+  return (
+    <div className="NavBar">
+      <button onClick={() => setVisible(!visible)}>
+        <img
+          alt="placeholder"
+          src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Feedbin-Icon-menu.svg"
+        />
+      </button>
+      {visible && <div>{list}</div>}
+    </div>
+  );
 };
 
-const HeadBox = ({ content }) => {
-  const [visible, setVisible] = useState(false);
-
+const HeadBox = (props) => {
   return (
     <div className="HeadBox">
-      <div className="hbox-1">
-        <button onClick={() => setVisible(!visible)}>
-          <img
-            alt="placeholder"
-            src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Feedbin-Icon-menu.svg"
-          />
-        </button>
-      </div>
+      <div className="hbox-1"></div>
       <div className="hbox-2">
-        <h1> {content.name} </h1>
-        {visible && (
-          <div>
-            <NavBar names={content.NavBar} links={content.NavBarLinks} />
-          </div>
-        )}
+        <h1> {props.name} </h1>
       </div>
     </div>
   );
@@ -52,10 +62,14 @@ const HeadBox = ({ content }) => {
 const TributePage = ({ content }) => {
   return (
     <div>
-      <HeadBox content={content} />
+      <NavBar links={content.NavBarLinks} names={content.NavBar} />
+
+      <HeadBox name={content.name} />
+
       <MainContent
         contentText={content.contentText}
         contentID={content.NavBarLinks}
+        contentImage={content.contentImages}
       />
     </div>
   );
@@ -65,7 +79,11 @@ export default function App() {
   return <TributePage content={CONTENT} />;
 }
 
+// Pages are dynamically generated based on which object is used for props.
+//contentText,NavBar,NavBarLinks, and contentImage should all have the same length and be arrays
+
 const CONTENT = {
+  contentImages: ["images/musk1.jpg", "images/tesla.jpg"],
   contentText: ["tesla is a car", "lorem Ipsum", "lots of money"],
   NavBar: ["elon", "musk", "tesla"],
   NavBarLinks: ["uno", "dos", "tres"],
@@ -75,6 +93,6 @@ const CONTENT = {
 const CONTENT2 = {
   contentText: ["apple technology", "lorem Ipsum", "lots of money"],
   NavBar: ["steve", "jobs", "apple"],
-  NavBarLinks: ["#testing", "#one", "#two"],
+  NavBarLinks: ["testing", "one", "two"],
   name: "Steve Jobs"
 };
